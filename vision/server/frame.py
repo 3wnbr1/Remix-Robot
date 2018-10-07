3bc8bc53da979064d04d4ef5c23105a8694e9227
3wnbr1 <ewen.brun@icloud.com>
2018-10-07 11:42:28 +0200

    Patching frame retrieval

--- vision/server/frame.py
+++ vision/server/frame.py
@@ -17,6 +17,7 @@ class Frame:
         self.image = image
         self.date = time.time()
         self.processed = False
+        self.retrieved = False
 
     def process(self):
         """Method to process the frame."""
@@ -25,7 +26,6 @@ class Frame:
             self.extentration = exentrationPercentage(self.image, self.rect)
             self.processed = True
             self.distance = distanceToObject(self.rect)
-            self.forward = None
         else:
             self.extentration = None
             self.processed = True
@@ -34,7 +34,9 @@ class Frame:
 
     def giveDirection(self):
         """Giving direction to the robot. -> v_right, v_left, time."""
-        if self.rect is not None:
+
+        if self.rect is not None and is not self.retrieved:
             if self.distance < DISTANCE_THRESHOLD or abs(self.extentration) > EXENTRATION_THRESHOLD:
+                self.retrieved = True
                 return str(self.extentration) + "," + str(self.distance) + ",1"
         return "0,0,0"
